ecf4fd2e2594f090582da3695815405a60e78b77
thiagofddias <thiagofdias2@gmail.com>
2024-11-26 15:12:52 -0300

    session 5 complete

---
+++ pages/alunos/[id].jsx
@@ -0,0 +1,39 @@
+export async function getStaticPaths() {
+    const resp = await fetch(`http://localhost:3000/api/alunos/tutores`);
+    const ids = await resp.json();
+
+    const paths = ids.map(id => {
+        return { params: { id: `${id}` }}
+    })
+    
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const resp = await fetch(`http://localhost:3000/api/alunos/${context.params.id}`);
+    const aluno = await resp.json();
+
+    return {
+        props: {
+            aluno
+        }
+    }
+}
+
+export default function AlunoPorId() {
+    const { aluno } = props;
+
+    return (
+        <div>
+            <h1>Detalhes do Aluno</h1>
+            <ul>
+                <li>{aluno.id}</li>
+                <li>{aluno.nome}</li>
+                <li>{aluno.email}</li>
+            </ul>
+        </div>
+    )
+}
---
+++ pages/estatico4.jsx
@@ -0,0 +1,32 @@
+export async function getStaticProps() {
+    console.log('[Server] Gerando props para o componente...');
+    const resp = await fetch('http://localhost:3000/api/produtos');
+    const produtos = await resp.json();
+
+    return {
+        props: {
+            produtos
+        }
+    }
+}
+
+export default function Estatico4(props) {
+    function renderizarProdutos() {
+        return props.produtos.map(produto => {
+            return (
+                <li key={produto.id}>
+                    {produto.id} - {produto.nome} tem preco de R${produto.preco}
+                </li>
+            )
+        })
+    }
+
+    return (
+        <div>
+            <h1>Estatico #04</h1>
+            <ul>
+                {renderizarProdutos()}
+            </ul>
+        </div>
+    )
+}
